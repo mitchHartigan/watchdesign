c63cebfa553dabcd95348e06e978acaed7bf571b
Mitch Hartigan <42709932+mitchHartigan@users.noreply.github.com>
2025-04-05 16:27:31 -0700

    content: remove free trial link

--- src/index.js
+++ src/index.js
@@ -4,8 +4,6 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "./index.css";
 
-//info.structurestudios.com/e3t/Ctc/T4+113/c2Ln904/MWJFGYrCpq1W3sHTn891NJVVW8Ny6Rt5v1lm4N6D7jYb3qgyTW7lCdLW6lZ3ktW7Qt64N7t-271W6_7yMb76YQzvW2FQZMv7L_-HmW1H0lG68HqFGpW5s495P3JyHc3W5Ggwkw3lkkvqW6db_QZ4mKpR0W39TmtL4J0mnlW7mWwX_1zlgCWW1-76n-2Y4ttfW1yZfVB7gBgD_W6XgNg-69jJ9tW4rXBXH2skDkyW1gx_-s2tGddwW2XstNh78g-V1W2CXKCb3stNgHW7DHt578H0f11W5fzXZ24m6dVzVm-hfx5_QDwHW5Vdq6J5V0yJbW3r70s77Zfn_JW2QNkrd1-LtXWW2X0w1c64RnwRW10n3f07ZK-jYf135r4H04
-
 https: const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
